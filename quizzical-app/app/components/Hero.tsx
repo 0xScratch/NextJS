0be42a349127f827f2920bd7df126f4722a27e0c
Aryan Malik <aryanhunny98@gmail.com>
2023-08-03 18:32:13 +0530

    Navigation added in quizzical-app

--- quizzical-app/app/components/Hero.tsx
+++ quizzical-app/app/components/Hero.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
+
 export default function Hero() {
     return (
         <>
             <div className="flex flex-col items-center">
                 <h1 className="text-3xl mb-2 text-[#293264]">Quizzical</h1>
                 <p className="text-sm mb-6 text-[#293264]">Some description if needed</p>
-                <button className="bg-[#4d5b9e] text-[#f5f7fb] px-12 py-3 text-xs font-bold rounded-xl">Start quiz</button>
+                <button className="bg-[#4d5b9e] text-[#f5f7fb] px-12 py-3 text-xs font-bold rounded-xl">
+                    <Link href="/quiz">Start quiz</Link>
+                </button>
             </div>
         </>
     )
